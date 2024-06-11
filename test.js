47a54b7fd9020b98aab1d54e8d7edfe01f35a02f
Satish Gupta <125724143+Satish0079@users.noreply.github.com>
2024-06-11 11:45:04 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,92 @@
+
+// x=2;
+// y=2;
+// z= x+y;
+// console.log(`The sum of x and y is :${z}`);
+
+
+// x=20;
+// y=2;
+// z= x-y;
+// console.log(`The subtractiion of x and y is :${z}`);
+
+// Calculate area of rectangle
+
+// a =12;
+// b = 12;
+// c = a*b;
+
+// console.log(`The Area of the rectangle with length:${a} and breadth:${b} is ${c} `)
+
+
+
+// num1 = 12;
+// num2 = 15;
+
+// if(num1>num2){
+//     console.log(`Number ${num1} is greater than number ${num2}`)
+// }
+// else if(num2>num1){
+//     console.log(`Number ${num2} is greater than number ${num1}`)
+// }
+
+
+
+
+// num1 = 12;
+// num2 = 14;
+// num3 = 12;
+
+// if(num1>num2 && num1>num3){
+//     console.log(`${num1} is greater than ${num2} and ${num3}`)
+// }
+// else if(num2>num1 && num2>num3){
+//     console.log(`${num2} is greater than ${num1} and ${num3}`)
+// }
+// else{
+//     console.log(`${num3} is greater than ${num2} and ${num1}`)
+// }
+
+
+// Result Calculator
+
+// marks = 67;
+// if(marks>=35){
+//     console.log("Student is pass.");
+// }
+// else{
+//     Console.log("Student is Fail.");
+
+
+
+
+
+// let score = prompt("Enter your exam score (out of 500):");
+// score = Number(score);  
+
+// if (isNaN(score) || score < 0 || score > 500) {
+//     alert("Please enter a valid score between 0 and 500.");
+// } else if (score >= 250) {
+//     alert("Congratulations! You have passed.");
+// } else {
+//     alert("Sorry, you have failed.");
+//      }
+
+
+
+// Rank Predicter
+
+// maths = 45;
+// physics = 40;
+// chemistry = 56;
+
+// total_score = maths + physics + chemistry;
+// score = total_score/3;
+
+// if(score>=35){
+//     console.log("Student is passed");
+// }
+// else if(score)
+
+
+
---
+++ test2.js
@@ -0,0 +1,4 @@
+<script>
+    let arr = [25,45,56,78]
+    console.log(arr[0])
+</script>
